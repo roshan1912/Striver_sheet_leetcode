304fddca9b5ebac4412cb01f8587c0573a558b5d
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2022-04-12 21:37:32 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (55.02%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums[nums.length-1]<target){
+            return nums.length;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target || nums[i]>target){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
