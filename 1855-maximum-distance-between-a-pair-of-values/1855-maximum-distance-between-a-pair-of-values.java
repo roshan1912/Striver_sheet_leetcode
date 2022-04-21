0d478a6e8557934718086ffe1485decffba0b670
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2022-04-22 01:40:48 +0530

    Time: 3 ms (86.93%), Space: 103.1 MB (27.34%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int res = 0, i = 0, n = nums1.length, m = nums2.length;
+        for (int j = 0; j < m; ++j) {
+            while (i < n && nums1[i] > nums2[j])
+                i++;
+            if (i == n) break;
+            res = Math.max(res, j - i);
+        }
+        return res;
+    }
+}
