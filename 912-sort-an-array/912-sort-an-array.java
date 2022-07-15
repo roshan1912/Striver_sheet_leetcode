af157b037eea33de84de4d1cfa49827f04b4798a
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2022-07-15 23:22:59 +0530

    Time: 104 ms (14.23%), Space: 73.5 MB (11.64%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,13 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+        }
+        int id=0;
+        while(!pq.isEmpty()){
+            nums[id++]=pq.poll();
+        }
         return nums;
     }
 }
