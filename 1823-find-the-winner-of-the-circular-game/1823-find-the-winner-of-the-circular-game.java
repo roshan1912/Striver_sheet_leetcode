addaf85e9bb86e03e97d3461eb41d6b43a7e80d0
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2022-07-28 01:27:25 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.45%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+          return helper(n,k)+1;
+    }
+    public int helper(int size, int k){
+        if(size==1)return 0;
+            return (helper(size-1,k)+k)%size;
+    }
+}
