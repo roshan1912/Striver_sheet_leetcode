795742416c65dad7341db698a5a1d5bfde94ecd3
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2022-10-03 23:13:12 +0530

    Time: 1107 ms (5.00%), Space: 152.3 MB (21.51%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -7,7 +7,7 @@ class Solution {
         }
         int max=0;
         for(int num:nums){
-            if(!set.contains(num-1)){
+            if(!set.contains(num-1)){// we need tp check for previous element in set because if the element is not the smallest we might have to iterate for n^2 time which will give TLE eventually.
                 int curr = num;
                 int count =1;
                 while(set.contains(curr+1)){
